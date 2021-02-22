05a22c32a992fbe48195bafee1c6bd1677c1c83e
Chrybear <50463991+Chrybear@users.noreply.github.com>
2021-02-22 15:16:45 -0500

    Initial Commit

---
+++ Animated Satellite Images/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animated Satellite Images
+//
+//  Created by user186035 on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
